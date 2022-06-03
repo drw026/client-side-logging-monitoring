f5fd80b3105cc4a1d2c09e82b1c0b45de0357656
Incentro Interactive <incentrointeractive@MacBook-Pro-van-Incentro.local>
2022-06-03 16:11:42 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+    node: 'http://localhost:9200',
+    auth: {
+        username: 'elastic',
+        password: 'test',
+      },
+})
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.post('/error', async (req, res) => {
+    const result = await client.index({
+        index: 'test',
+        body: {
+            title: req.body.title,
+            author: req.body.author,
+            content: req.body.content,
+          },
+    });
+
+    res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
