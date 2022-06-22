9fd653b1b8d25226233a7fcd90ee5663258663b5
Andrew Reasoa <andrew.reasoa@incentro..com>
2022-06-22 21:57:52 +0200

    send to elasticsearch

---
+++ pages/js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('error', function(event) {
+    fetch('/error', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: event.error.message }),
+    }).then();
+});
+
+const firstButton = document.querySelector('#button1');
+firstButton.addEventListener('click', () => buttonHandler('Custom error 1'));
+
+const secondButton = document.querySelector('#button2');
+secondButton.addEventListener('click', () => buttonHandler('Custom error 2'));
+
+function buttonHandler (message) {
+    throw new Error(message);
+}
+
+function init () {
+    if (navigator.userAgent.indexOf('Googlebot') > -1) {
+        throw new Error('Googlebot entered');
+    }
+}
+
+init();
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const { Client } = require('@elastic/elasticsearch')
+const { Client } = require('@elastic/elasticsearch');
 const client = new Client({
     node: 'http://localhost:9200',
     auth: {
@@ -16,17 +16,37 @@ app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'pages')))
 
+const sendToElasticsearch = async (index, entry) => {
+    const date = new Date();
+    const month = String(date.getMonth()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    await client
+        .index({
+            index: `${index}-${date.getFullYear()}.${month}.${day}`,
+            body: entry,
+        })
+        .catch((error) => console.log(error.message));
+};
+
 app.post('/error', async (req, res) => {
-    const result = await client.index({
-        index: 'test',
-        body: {
-            title: req.body.title,
-            author: req.body.author,
-            content: req.body.content,
-          },
+    sendToElasticsearch('error', {
+        '@timestamp': new Date().toISOString(),
+        level: 'error',
+        message: req.body.message,
+        request: {
+            method: req.method,
+            url: req.url,
+            useragent: req.headers ? req.headers['user-agent'] : '',
+            clientIp: req.ip,
+        },
+        response: {
+            statusCode: res.statusCode,
+            responseTime: res.responseTime,
+        }
     });
 
-    res.send(result);
+    res.send('OK');
 });
 
 app.listen(port, () => {
