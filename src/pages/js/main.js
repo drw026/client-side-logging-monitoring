40b403484de19332d1ff808170f2316067f186c0
Andrew Reasoa <andrew.reasoa@incentro..com>
2022-06-24 15:19:09 +0200

    move error event listener to HEAD

--- src/pages/js/main.js
+++ src/pages/js/main.js
@@ -1,24 +1,14 @@
-window.addEventListener('error', function(event) {
-    fetch('/error', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            message: event.error.message,
-            context: {
-                page: window.location.href,
-                sourcefile: `${event.filename} - ${event.lineno} - ${event.colno}`,
-            }
-        }),
-    }).then();
-});
-
 const firstButton = document.querySelector('#button1');
 firstButton.addEventListener('click', () => buttonHandler('Custom error 1'));
 
 const secondButton = document.querySelector('#button2');
 secondButton.addEventListener('click', () => buttonHandler('Custom error 2'));
+
+const thirdButton = document.querySelector('#button3');
+thirdButton.addEventListener('click', () => {
+    const type = 'test';
+    console.log(typo);
+})
 
 function buttonHandler (message) {
     throw new Error(message);
